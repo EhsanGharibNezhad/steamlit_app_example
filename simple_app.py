883e56bacd7546c01be5ca4ab3195c4ffe380217
Ehsan Gharib-Nezhad <e.gharibnezhad@gmail.com>
2021-12-18 12:17:36 -0800

    Add files via upload

---
+++ simple_app.py
@@ -0,0 +1,13 @@
+import pickle
+import streamlit as st
+
+st.title("Which author do you write like?")
+
+with open("models/mnb_pipe.pkl", mode='rb') as pickle_in:
+    pipe = pickle.load(pickle_in)
+
+user_text = st.text_input("Please write some text:")
+
+prediction = pipe.predict([user_text])[0] # We're using [0] to index the array and get only the string
+
+st.write(f"You write like {prediction}.")
